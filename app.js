4902dcbc3fe6e1930974bf164b1f1e6befc78e39
Hoang Duy Quang <quang9514@gmail.com>
2022-09-04 11:53:54 +0700

    heroku build

--- app.js
+++ app.js
@@ -82,6 +82,8 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
+            console.log("Server is running.");
+        });
     })
     .catch(err => console.log(err));
