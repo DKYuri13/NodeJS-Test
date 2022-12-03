807e85332ea83aae8f3a219c0796b8c12bb9b868
Hoang Duy Quang <quang9514@gmail.com>
2022-12-03 20:47:18 +0700

    second commit

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ app.use(errorController.get404);    //Xử lý các link không tồn tại
 mongoose                            //Connect DB và host/port
     .connect(MONGODB_URI)
     .then(result => {
-        app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
-            console.log("Server is running.");
-        });
-        // app.listen(3000)
+        // app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
+        //     console.log("Server is running.");
+        // });
+        app.listen(3000)
     })
     .catch(err => console.log(err));
